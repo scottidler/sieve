61b27cf5f8b0bf53da46be9b551cde36387e1173
Scott A. Idler <scott.a.idler@gmail.com>
2022-12-16 16:07:39 -0800

    added logging and logic to prevent modifying if the labels are already set

--- sieve.py
+++ sieve.py
@@ -40,12 +40,18 @@ from leatherman.fuzzy import fuzzy, FuzzyList
 from leatherman.repr import __repr__
 from leatherman.dbg import dbg
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-file_handler = logging.FileHandler('sieve.log')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'INFO').upper()
+
+logger = logging.getLogger('sieve')
+logging.getLogger('googleapiclient').setLevel(logging.ERROR)
+logging.basicConfig(
+    format='%(asctime)s|%(name)s|%(message)s',
+    level=LOGGING_LEVEL,
+    handlers=[
+        logging.FileHandler('sieve.log'),
+        logging.StreamHandler()
+    ]
+)
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = [
@@ -78,6 +84,17 @@ LABELS = {
     'UNREAD': 'UNREAD',
     'STARRED': 'STARRED',
     'IMPORTANT': 'IMPORTANT',
+    # 'CATEGORY_FORUMS': 'CATEGORY_FORUMS',
+    # 'CATEGORY_SOCIAL': 'CATEGORY_SOCIAL',
+    # 'CATEGORY_UPDATES': 'CATEGORY_UPDATES',
+    # 'CATEGORY_FINANCE': 'CATEGORY_FINANCE',
+    # 'CATEGORY_PERSONAL': 'CATEGORY_PERSONAL',
+    # 'CATEGORY_SHOPPING': 'CATEGORY_SHOPPING',
+    # 'CATEGORY_PURCHASES': 'CATEGORY_PURCHASES',
+    # 'CATEGORY_PROMOTIONS': 'CATEGORY_PROMOTIONS',
+}
+
+CATEGORIES = {
     'CATEGORY_FORUMS': 'CATEGORY_FORUMS',
     'CATEGORY_SOCIAL': 'CATEGORY_SOCIAL',
     'CATEGORY_UPDATES': 'CATEGORY_UPDATES',
@@ -136,9 +153,19 @@ if os.path.islink(__file__):
 
 NAME, EXT = os.path.splitext(REAL_NAME)
 
+class LabelIntersectionError(Exception):
+    def __init__(self, addLabelIds, removeLabelIds):
+        msg = f'error: addLabelIds={addLabelIds} and removeLabelIds={removeLabelIds} intersect'
+        super().__init__(msg)
+
 class SieveYmlNotFoundError(Exception):
     def __init__(self, sieve_yml):
         msg = f'error: sieve_yml={sieve_yml} not found'
+        super().__init__(msg)
+
+class InvalidLabelIdError(Exception):
+    def __init__(self, labelIds):
+        msg = f'error: labelId={labelIds} is not a valid labelId'
         super().__init__(msg)
 
 def pf(obj):
@@ -148,6 +175,9 @@ def pf(obj):
 
 def pp(obj):
     print(pf(obj))
+
+def ppl(obj):
+    logger.info(pf(obj))
 
 def tuplify(obj):
     if obj is None:
@@ -168,18 +198,42 @@ def format_emails(s, regex=re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z
         return tuple(regex.findall(s))
     return ()
 
+def is_valid(obj):
+    return len(set(obj.addLabelIds) & set(obj.removeLabelIds)) == 0
+
+def difference(minuend, subtrahend):
+    minuend = set(minuend)
+    subtrahend = set(subtrahend)
+    difference = tuple(minuend - subtrahend)
+    return difference
+
+def intersect(a, b):
+    a = set(a)
+    b = set(b)
+    intersection = tuple(a & b)
+    return intersection
+
+def union(a, b):
+    a = set(a)
+    b = set(b)
+    union = tuple(a | b)
+    return union
+
+def filter_category_labels(labelIds):
+    return tuple([labelId for labelId in labelIds if labelId not in CATEGORIES])
+
 class Message:
-    def __init__(self, thread, id, historyId, internalDate, labelIds, payload, sizeEstimate, snippet, threadId, raw=None):
-        self.id = id
+    def __init__(self, thread, id, labelIds, threadId, historyId=None, internalDate=None, sizeEstimate=None, snippet=None, payload=None, raw=None):
         self.thread = thread
+        self.id = id
+        self.labelIds = filter_category_labels(labelIds)
+        self.threadId = threadId
         self.historyId = historyId
         self.internalDate = internalDate
-        self.labelIds = labelIds
-        self.payload = Addict(payload)
-        self.raw = raw
         self.sizeEstimate = sizeEstimate
         self.snippet = snippet
-        self.threadId = threadId
+        self.payload = Addict(payload if payload else {})
+        self.raw = raw
 
     __repr__ = __repr__
 
@@ -229,7 +283,7 @@ class Message:
     def labels(self):
         '''labels: plural'''
         return {
-            self.thread.sieve.labels[id]: id
+            self.thread.sieve.ids_to_labels[id]: id
             for id
             in self.labelIds
         }
@@ -245,11 +299,11 @@ class Message:
         }
 
 class Thread:
-    def __init__(self, sieve, id, historyId, messages, snippet=None):
+    def __init__(self, sieve, id, messages, historyId=None, snippet=None):
         self.id = id
         self.sieve = sieve
-        self.historyId = historyId
         self.messages = [Message(thread=self, **message) for message in messages]
+        self.historyId = historyId
         self.snippet = snippet
 
     __repr__ = __repr__
@@ -257,11 +311,11 @@ class Thread:
     @property
     def labels(self):
         '''labels: plural'''
-        return tuple(tuple(m.labelIds) for m in self.messages)
+        return [m.labelIds for m in self.messages]
 
     @property
-    def are_labels_uniform(self):
-        '''are_labels_uniform: singular'''
+    def labels_are_uniform(self):
+        '''labels_are_uniform: singular'''
         return len(set(self.labels)) == 1
 
     @property
@@ -301,26 +355,54 @@ class Thread:
 
     def labels_match(self, labels):
         '''labels_match: singular'''
-        if self.are_labels_uniform:
+        if self.labels_are_uniform:
             return self.labels[0] == labels
         return False
 
-class Filter(Addict):
-    def __init__(self, sieve=None, name=None, subject=None, sender=None, fr=None, to=None, cc=None, bcc=None, precedence=None, addLabelIds=None, removeLabelIds=None, **headers):
-        dict.__init__(
-            self,
-            name=name,
-            subject=subject,
-            sender=sender,
-            fr=tuplify(fr),
-            to=to,
-            cc=tuplify(cc),
-            bcc=tuplify(bcc),
-            precedence=precedence,
-            addLabelIds=[] if addLabelIds is None else addLabelIds,
-            removeLabelIds=[] if removeLabelIds is None else removeLabelIds,
-            **headers,
-        )
+    def is_uptodate(self, body):
+        def is_uptodate(labels, body):
+            if len(intersect(labels, body.addLabelIds)) != len(body.addLabelIds):
+                logger.debug(f'ADD: intersect failed: labels={labels} body.addLabelIds={body.addLabelIds} add_result={intersect(labels, body.addLabelIds)}')
+                return False
+            if len(intersect(labels, body.removeLabelIds)) != 0:
+                logger.debug(f'REMOVE: intersect failed: labels={labels} body.removeLabelIds={body.removeLabelIds} remove_result={intersect(labels, body.removeLabelIds)}')
+                return False
+            return True
+        return all(is_uptodate(labels, body) for labels in self.labels)
+
+class Filter:
+    def __init__(self, name=None, subject=None, sender=None, fr=None, to=None, cc=None, bcc=None, precedence=None, addLabelIds=None, removeLabelIds=None, **headers):
+        self.name = name
+        self.subject = subject
+        self.sender = sender
+        self.fr = tuplify(fr)
+        self.to = to
+        self.cc = tuplify(cc)
+        self.bcc = tuplify(bcc)
+        self.precedence = precedence
+        self.addLabelIds = tuple([] if addLabelIds is None else set(addLabelIds))
+        self.removeLabelIds = tuple([] if removeLabelIds is None else set(removeLabelIds))
+        self.headers = headers
+
+        if not is_valid(self):
+            raise LabelIntersectionError(self.addLabelIds, self.removeLabelIds)
+
+    __repr__ = __repr__
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'subject': self.subject,
+            'sender': self.sender,
+            'fr': self.fr,
+            'to': self.to,
+            'cc': self.cc,
+            'bcc': self.bcc,
+            'precedence': self.precedence,
+            'addLabelIds': tuple(self.addLabelIds),
+            'removeLabelIds': tuple(self.removeLabelIds),
+            'headers': self.headers,
+        }
 
 class Change:
     def __init__(self, sieve, thread, filters):
@@ -332,10 +414,32 @@ class Change:
 
     def execute(self):
         body = Addict(addLabelIds=[], removeLabelIds=[])
+        logger.info(f'thread.id={self.thread.id}: "{self.thread.subject}"')
+        logger.debug(f'thread.labels={self.thread.labels}')
         for f in self.filters:
-            for labelId in self.filter.addLabelIds:
-                if labelId not in self.thread.labels:
-                    body.addLabelIds += labelId
+            if any([id.startswith('_/') for id in f.addLabelIds]):
+                raise InvalidLabelIdError(f.addLabelIds)
+            if any([id.startswith('_/') for id in f.removeLabelIds]):
+                raise InvalidLabelIdError(f.removeLabelIds)
+            body.addLabelIds += f.addLabelIds
+            body.removeLabelIds += f.removeLabelIds
+            if not is_valid(body):
+                raise LabelIntersectionError(body.addLabelIds, body.removeLabelIds)
+        logger.debug(f'after collecting filters:')
+        logger.debug(f'body={pf(body)}')
+        body2 = self.sieve.body_ids_to_labels(body)
+        logger.debug(f'body2={pf(body2)}')
+
+        if self.thread.is_uptodate(body):
+            logger.debug(f'thread is up to date, already has changes {pf(body2)}')
+        else:
+            logger.info(f'thread needs changes {pf(body2)}')
+            try:
+                self.sieve.threads_api.modify(userId='me', id=self.thread.id, body=body).execute()
+                logger.info('completed successfullly')
+            except HttpError as e:
+                logger.info(f'Error executing actions={body} for thread_id={thread_id}')
+                raise e
 
 class Sieve:
     def __init__(self, creds_json, sieve_yml, **kwargs):
@@ -351,12 +455,26 @@ class Sieve:
     __repr__ = __repr__
 
     @property
-    def labels(self):
+    def labels_to_ids(self):
         return {
             label['name']: label['id']
             for label
             in self.labels_api.list(userId='me').execute()['labels']
         }
+
+    @property
+    def ids_to_labels(self):
+        return {
+            label['id']: label['name']
+            for label
+            in self.labels_api.list(userId='me').execute()['labels']
+        }
+
+    def body_ids_to_labels(self, body):
+        return Addict(
+            addLabelIds=tuple(self.ids_to_labels[id] for id in body.addLabelIds),
+            removeLabelIds=tuple(self.ids_to_labels[id] for id in body.removeLabelIds)
+        )
 
     def auth(self, creds_json):
         creds = None
@@ -386,8 +504,8 @@ class Sieve:
                 body.removeLabelIds += [REMOVE_ACTION_TO_LABEL[action]]
             elif action in ADD_ACTION_TO_LABEL:
                 body.addLabelIds += [ADD_ACTION_TO_LABEL[action]]
-            elif action in self.labels:
-                body.addLabelIds += [self.labels[action]]
+            elif action in self.labels_to_ids:
+                body.addLabelIds += [self.labels_to_ids[action]]
             else:
                 try:
                     result = self.labels_api.create(userId='me', body={'name': action}).execute()
@@ -395,7 +513,7 @@ class Sieve:
                 except HttpError as e:
                     print(f'Error creating label="{action}"')
                     if e.resp.status == 409:
-                        body.addLabelIds += [self.labels[action]]
+                        body.addLabelIds += [self.labels_to_ids[action]]
                     else:
                         raise e
         return body
@@ -470,7 +588,7 @@ class Sieve:
         self.cfg = cfg
 
     def show_filters(self):
-        pp(self.filters)
+        pp([f.to_json() for f in self.filters])
 
     def filter_thread(self, thread):
         filters = []
@@ -501,7 +619,7 @@ class Sieve:
         while threads_req:
             threads_res = threads_req.execute()
             threads = threads_res.get('threads', [])
-            print('len(threads) =', len(threads))
+            logger.info(f'len(threads)={len(threads)}')
             for thread in threads:
                 thread = Thread(sieve=self, **self.threads_api.get(userId='me', id=thread['id'], format='metadata', metadataHeaders=METADATA_HEADERS).execute())
                 changes += self.filter_thread(thread)
@@ -509,21 +627,15 @@ class Sieve:
             threads_req = self.threads_api.list_next(threads_req, threads_res)
         return changes
 
-    def execute_actions(self, changes):
+    def execute_changes(self, changes):
         for change in changes:
             change.execute()
-            # try:
-            #     self.threads_api.modify(userId='me', id=thread_id, body=body).execute()
-            # except HttpError as e:
-            #     print(f'Error executing actions={actions} for thread_id={thread_id}')
-            #     raise e
-            # print(f'thread={thread_id} processes')
-            # print(80*'-')
+            logger.info('*'*80)
 
     def run(self):
-        pp(self.labels)
+        ppl(self.labels_to_ids)
         changes = self.filter_gmail()
-        self.execute_actions(changes)
+        self.execute_changes(changes)
 
 def main(args):
     parser = ArgumentParser()
